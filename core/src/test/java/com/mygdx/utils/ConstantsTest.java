e670878248231893a4e0e108561e69ca5e52fa28
Jacob Poulton <jp1808@york.ac.uk>
2022-02-18 12:25:14 +0000

    Working on setting up tests (not done).

---
+++ core/src/test/java/com/mygdx/utils/ConstantsTest.java
@@ -0,0 +1,20 @@
+package com.mygdx.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstantsTest {
+
+    @Test
+    void INIT_CONSTANTS() {
+        System.out.println("a");
+        fail();
+    }
+
+    @Test
+    void UPDATE_VIEWPORT() {
+        System.out.println("b");
+        assertTrue(true);
+    }
+}
