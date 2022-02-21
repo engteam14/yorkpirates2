0f2e3cf1a60baf8ff62b06b75a4c1874ae3fcdb6
katie291100 <kmm565@york.ac.uk>
2022-02-21 20:11:28 +0000

    resolves #4 create QueueFIFOTest

---
+++ core/src/test/java/com/mygdx/utils/QueueFIFOTest.java
@@ -0,0 +1,88 @@
+package com.mygdx.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QueueFIFOTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void testToArray() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void containsAll() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void retainAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void offer() {
+    }
+
+    @Test
+    void testRemove1() {
+    }
+
+    @Test
+    void pop() {
+    }
+
+    @Test
+    void poll() {
+    }
+
+    @Test
+    void element() {
+    }
+
+    @Test
+    void peek() {
+    }
+}
