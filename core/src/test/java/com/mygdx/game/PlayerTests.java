9bdc9b0dc70947390680af82921536348be5ff79
Jacob Poulton <jp1808@york.ac.uk>
2022-03-04 00:13:12 +0000

    Setting up base for game testing File for player related tests made Gradle build files updated for running the tests

---
+++ core/src/test/java/com/mygdx/game/PlayerTests.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class PlayerTests {
+
+	@Test
+	public void playerMove() {
+
+	}
+}
