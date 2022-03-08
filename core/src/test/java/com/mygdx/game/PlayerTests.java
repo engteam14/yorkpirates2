2164768646361a7169ac395cbf2213a576bf4551
jacobpoulton <jp1808@york.ac.uk>
2022-03-08 22:38:01 +0000

    refactor rendering (x1) commit split due to codewithme

--- core/src/test/java/com/mygdx/game/PlayerTests.java
+++ core/src/test/java/com/mygdx/game/PlayerTests.java
@@ -1,13 +1,33 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.Entitys.Ship;
+import com.mygdx.game.Managers.PhysicsManager;
+import com.mygdx.game.Managers.RenderingManager;
+import com.mygdx.game.Managers.ResourceManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static com.mygdx.utils.Constants.INIT_CONSTANTS;
 
 @RunWith(GdxTestRunner.class)
 public class PlayerTests {
 
 	@Test
 	public void playerMove() {
+		int id_ship = ResourceManager.addTexture("ship.png");
+		int id_map = ResourceManager.addTileMap("Map.tmx");
+		int atlas_id = ResourceManager.addTextureAtlas("Boats.txt");
+		int extras_id = ResourceManager.addTextureAtlas("UISkin/skin.atlas");
+		int buildings_id = ResourceManager.addTextureAtlas("Buildings.txt");
+		ResourceManager.addTexture("menuBG.jpg");
+		ResourceManager.addTexture("Chest.png");
+		ResourceManager.loadAssets();
 
+		INIT_CONSTANTS();
+		PhysicsManager.Initialize(false);
+
+		Ship ship = new Ship();
 	}
 }
