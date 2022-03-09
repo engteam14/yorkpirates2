eef5cdd47b5bd742299508b3fcf36de452d8bd0f
jacobpoulton <jp1808@york.ac.uk>
2022-03-09 00:30:11 +0000

    First game test shipMove test

--- core/src/test/java/com/mygdx/game/ShipTests.java
+++ core/src/test/java/com/mygdx/game/ShipTests.java
@@ -1,21 +1,24 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
-import com.mygdx.game.Entitys.Ship;
+import com.mygdx.game.Components.ComponentType;
+import com.mygdx.game.Components.RigidBody;
+import com.mygdx.game.Entitys.Player;
 import com.mygdx.game.Managers.PhysicsManager;
-import com.mygdx.game.Managers.RenderingManager;
 import com.mygdx.game.Managers.ResourceManager;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
 
 import static com.mygdx.utils.Constants.INIT_CONSTANTS;
 
 @RunWith(GdxTestRunner.class)
-public class PlayerTests {
+public class ShipTests {
 
-	@Test
-	public void playerMove() {
+	@Before
+	public void init(){
 		int id_ship = ResourceManager.addTexture("ship.png");
 		int id_map = ResourceManager.addTileMap("Map.tmx");
 		int atlas_id = ResourceManager.addTextureAtlas("Boats.txt");
@@ -27,7 +30,23 @@ public class PlayerTests {
 
 		INIT_CONSTANTS();
 		PhysicsManager.Initialize(false);
+	}
 
-		Ship ship = new Ship();
+	@Test
+	public void shipMove() {
+		Player player = new Player();
+		RigidBody playerRb = (RigidBody) player.getComponent(ComponentType.RigidBody);
+
+		Vector2 startPos = playerRb.getPosition().cpy();
+
+		for (int i = 0; i < 500; i++) {
+			playerRb.setVelocity(new Vector2(1f,1f));
+			PhysicsManager.update();
+		}
+
+		Vector2 endPos = playerRb.getPosition().cpy();
+
+		assertNotEquals("Ship did not move (x axis)", endPos.x, startPos.x);
+		assertNotEquals("Ship did not move (y axis)", endPos.y, startPos.y);
 	}
 }
