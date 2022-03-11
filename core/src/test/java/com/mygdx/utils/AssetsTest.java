0e1ea32c543cb3fa8795710feee670dac8a53b55
katie291100 <kmm565@york.ac.uk>
2022-03-11 14:27:28 +0000

    Added tests to all current assets.

--- core/src/test/java/com/mygdx/utils/AssetsTest.java
+++ core/src/test/java/com/mygdx/utils/AssetsTest.java
@@ -1,17 +1,90 @@
 package com.mygdx.utils;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.math.Vector2;
+
+import com.mygdx.game.GdxTestRunner;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
+import java.io.File;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class AssetsTest {
+
+@RunWith(GdxTestRunner.class)
+
+public class AssetsTest {
+    @Test
+    void ArialFontAsset() {
+        assertTrue(new File("assets/Arial.ttf").isFile(), "Arial.ttf font Missing");
+    }
 
     @Test
-    void playerShip() {
-        assertAll(() -> assertNotNull(Gdx.files.internal("core/assets/ship.png"), "Ship Sprite Missing"));
+    void BeachTilesetPng() {
+        assertTrue(new File("assets/Beach Tileset.png").isFile(), "Beach Tileset.png Missing");
+    }
+
+    @Test
+    void BeachTilesetAssetTsx() {
+        assertTrue(new File("assets/Beach Tileset.tsx").isFile(), "Beach Tileset.tsx Missing");
+    }
+    @Test
+    void BoatsAssetPNG() {
+        assertTrue(new File("assets/boats.png").isFile(), "boats.png Sprite Missing");
+    }
+    @Test
+    void BoatsAssetTxt() {
+        assertTrue(new File("assets/Boats.txt").isFile(), "boats.txt file Missing");
+    }
+
+    @Test
+    void BuildingsAssetTxt() {
+        assertTrue(new File("assets/Buildings.txt").isFile(), "buildings.txt Missing");
+    }
+    @Test
+    void ChestAssetPng(){
+        assertTrue(new File("assets/Chest.png").isFile(), "Chest Sprite Missing");
+    }
+
+    @Test
+    void GSettingsAssetJson() {
+        assertTrue(new File("assets/GameSettings.json").isFile(), "GameSettings.json Missing");
+    }
+    @Test
+    void mapTmxAsset() {
+        assertTrue(new File("assets/Map.tmx").isFile(), "Map.tmx Missing");
+    }
+    @Test
+    void menuBGAsset() {
+        assertTrue(new File("assets/MenuBG.jpg").isFile(), "MenuBG.jpg Missing");
+    }
+    @Test
+    void OtherPngAsset() {
+        assertTrue(new File("assets/other.png").isFile(), "other.png Missing");
+    }
+    @Test
+    void playerShipAsset() {
+        assertTrue(new File("assets/ship.png").isFile(), "Ship.png Sprite Missing");
+    }
+    @Test
+    void SkinDefaultFnt() {
+        assertTrue(new File("assets/UISkin/default.fnt").isFile(), "UISkin/default.fnt Missing");
+    }
+    @Test
+    void SkinDefaultPng() {
+        assertTrue(new File("assets/UISkin/default.png").isFile(), "UISkin/default.png Missing");
+    }
+    @Test
+    void skinAtlasAsset() {
+        assertTrue(new File("assets/UISkin/skin.atlas").isFile(), "UISkin/skin.atlas Missing");
+    }
+    @Test
+    void skinJsonAsset() {
+        assertTrue(new File("assets/UISkin/skin.json").isFile(), "UISkin/skin.json Missing");
+    }
+    @Test
+    void UiSkinJsonAsset() {
+        assertTrue(new File("assets/UISkin/uiskin.png").isFile(), "UISkin/uiskin.png Missing");
     }
 }
