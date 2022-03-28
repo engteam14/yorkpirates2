c613762e58556c536bc5bff92d153edd449b064c
jacobpoulton <jp1808@york.ac.uk>
2022-03-28 18:48:24 +0100

    Created a basic PowerUp class with methods to enable/disable as well as check whether it needs to

---
+++ core/src/main/java/com/mygdx/game/PowerUps/PowerUpOperation.java
@@ -0,0 +1,28 @@
+package com.mygdx.game.PowerUps;
+
+public enum PowerUpOperation {
+    /**
+     * Replace the key value with a new value.
+     */
+    replace,
+
+    /**
+     * Increment the key value by an amount.
+     */
+    increment,
+
+    /**
+     * Decrement the key value by an amount.
+     */
+    decrement,
+
+    /**
+     * Multiply the key value by an amount.
+     */
+    multiply,
+
+    /**
+     * Divide the key value by an amount.
+     */
+    divide
+}
